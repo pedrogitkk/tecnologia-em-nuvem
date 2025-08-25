b88d54ee1cc46a0a6c3a7810de0c87d80d11f191
pedrogitkk <pedro.tristao@etec.sp.gov.br>
2025-08-25 10:26:04 -0300

    Create index.php

---
+++ app2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FOrmulario de contatoi</title>
+</head>
+<body>
+    <h1>Fale conosco</h1>
+
+    <form action="" method="post">
+        <label for="nome">Nome</label>
+        <input type="text" name="nome" id="nome" required><br>
+
+        <label for="email">Email</label>
+        <input type="text" name="email" id="email" required><br>
+    </form>
+</body>
+</html>
